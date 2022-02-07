4048b82c82a5693e22a2a4e9be6d66e92281377d
david83748 <david.vashchenko@gmail.com>
2022-02-07 11:24:13 +0200

    add linter, webpack, netlify

--- src/index.js
+++ src/index.js
@@ -1,37 +1,30 @@
 const array = [
-    [3, 0, 1, 0, 0, 0,],
-    [0, 0, 0, 1, 0, 0,],
-    [0, 0, 0, 0, 0, 0,],
-    [0, 1, 0, 0, 1, 0,],
-    [0, 0, 0, 0, 0, 0,],
-    [0, 0, 1, 0, 0, 0,]
+  [3, 0, 1, 0, 0, 0],
+  [0, 0, 0, 1, 0, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 0, 1, 0],
+  [0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 0],
 ];
 function render(cells) {
-    let markup = '';
-    cells.forEach((row) => {
-        markup = markup + '<div class="row">'
-        row.forEach((item) => {
-            let block;
+  let markup = '';
+  cells.forEach((row) => {
+    markup += '<div class="row">';
+    row.forEach((item) => {
+      let block;
 
-            if(item == 0) {
-                block = 'path'
-
-            } else if (item == 1) {
-                block = 'tree'
-            
-            }else if (item == 3) {
-                block = 'person'
-
-            };
-            markup = markup + `<div class=${block}></div>`
-        });
-        markup = markup + '</div>'
+      if (item === 0) {
+        block = 'path';
+      } else if (item === 1) {
+        block = 'tree';
+      } else if (item === 3) {
+        block = 'person';
+      }
+      markup = `${markup}<div class=${block}></div>`;
     });
-    return markup
-    
+    markup += '</div>';
+  });
+  return markup;
+}
 
-
-};
-
-document.getElementById("wrapper").innerHTML = render(array)
-
+document.getElementById('wrapper').innerHTML = render(array);
