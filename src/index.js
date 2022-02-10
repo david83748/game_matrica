e1843e73e95e84868831a1e9d5bfc35138b7669f
david83748 <david.vashchenko@gmail.com>
2022-02-10 23:17:20 +0200

    created the movements of the red square

--- src/index.js
+++ src/index.js
@@ -1,24 +1,29 @@
 const array = [
-  [3, 0, 1, 0, 0, 0],
-  [0, 0, 0, 1, 0, 0],
-  [0, 0, 0, 0, 0, 0],
-  [0, 1, 0, 0, 1, 0],
-  [0, 0, 0, 0, 0, 0],
+  [0, 0, 1, 0, 0, 1],
+  [1, 3, 0, 1, 0, 0],
   [0, 0, 1, 0, 0, 0],
+  [0, 0, 0, 0, 1, 0],
+  [1, 0, 0, 0, 0, 1],
+  [0, 0, 1, 0, 0, 1],
 ];
+
+let currentStateRow = 0;
+let currentStateColumn = 0;
+
 function render(cells) {
   let markup = '';
-  cells.forEach((row) => {
+  cells.forEach((row, indexRow) => {
     markup += '<div class="row">';
-    row.forEach((item) => {
+    row.forEach((item, indexColumn) => {
       let block;
-
-      if (item === 0) {
-        block = 'path';
+      if (item === 3) {
+        currentStateRow = indexRow;
+        currentStateColumn = indexColumn;
+        block = 'person';
       } else if (item === 1) {
         block = 'tree';
-      } else if (item === 3) {
-        block = 'person';
+      } else if (item === 0) {
+        block = 'path';
       }
       markup = `${markup}<div class=${block}></div>`;
     });
@@ -26,5 +31,45 @@ function render(cells) {
   });
   return markup;
 }
-
 document.getElementById('wrapper').innerHTML = render(array);
+
+document.addEventListener('keyup', (event) => {
+  if (event.key === 'ArrowRight') {
+    const countColumns = array[0].length;
+    if (array[currentStateRow][currentStateColumn + 1] !== 1) {
+      if (currentStateColumn < countColumns - 1) {
+        array[currentStateRow][currentStateColumn] = 0;
+        array[currentStateRow][currentStateColumn + 1] = 3;
+        document.getElementById('wrapper').innerHTML = render(array);
+      }
+    }
+  }
+  if (event.key === 'ArrowLeft') {
+    if (array[currentStateRow][currentStateColumn - 1] !== 1) {
+      if (currentStateColumn > 0) {
+        array[currentStateRow][currentStateColumn] = 0;
+        array[currentStateRow][currentStateColumn - 1] = 3;
+        document.getElementById('wrapper').innerHTML = render(array);
+      }
+    }
+  }
+  if (event.key === 'ArrowDown') {
+    const countRows = array.length;
+    if (array[currentStateRow + 1][currentStateColumn] !== 1) {
+      if (currentStateRow < countRows - 1) {
+        array[currentStateRow][currentStateColumn] = 0;
+        array[currentStateRow + 1][currentStateColumn] = 3;
+        document.getElementById('wrapper').innerHTML = render(array);
+      }
+    }
+  }
+  if (event.key === 'ArrowUp') {
+    if (array[currentStateRow - 1][currentStateColumn] !== 1) {
+      if (currentStateRow > 0) {
+        array[currentStateRow][currentStateColumn] = 0;
+        array[currentStateRow - 1][currentStateColumn] = 3;
+        document.getElementById('wrapper').innerHTML = render(array);
+      }
+    }
+  }
+});
